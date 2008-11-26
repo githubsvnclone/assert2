ceee1d4ad9028893217f7187fbcb625a258920e1
Kevin Miller <phlip@rubyforge.org>
2008-11-26 23:23:31 +0000

    more common utilities
    git-svn-id: http://assert2.rubyforge.org/svn@30 c6f06dc1-0964-42b6-8008-9d063af89333

--- ruby1.9/lib/assert2.rb
+++ ruby1.9/lib/assert2.rb
@@ -733,55 +733,6 @@ module Test; module Unit; module Assertions
   end
   
   #!doc!
-  def assert(*args, &block)
-  #  This assertion calls a block, and faults if it returns
-  #  +false+ or +nil+. The fault diagnostic will reflect the
-  #  assertion's complete source - with comments - and will
-  #  reevaluate the every variable and expression in the
-  #  block.
-  #
-  #  The first argument can be a diagnostic string:
-  #
-  #    assert("foo failed"){ foo() }
-  #
-  #  The fault diagnostic will print that line.
-  # 
-  #  The next time you think to write any of these assertions...
-  #  
-  #  - +assert+
-  #  - +assert_equal+
-  #  - +assert_instance_of+
-  #  - +assert_kind_of+
-  #  - +assert_operator+
-  #  - +assert_match+
-  #  - +assert_not_nil+
-  #  
-  #  use <code>assert{ 2.1 }</code> instead.
-  #
-  #  If no block is provided, the assertion calls +assert_classic+,
-  #  which simulates RubyUnit's standard <code>assert()</code>.
-  #  
-  #  Note: This only works for Ruby 1.9, because it uses the Ripper library,
-  #  maintained by Ruby's core team.
-  #
-    if block
-      assert_ *args, &block
-    else
-      assert_classic *args
-    end
-    return true # or die trying ;-)
-  end
-
-  # This is a copy of the classic assert, so your pre-existing
-  # +assert+ calls will not change their behavior
-  #
-  def assert_classic(boolean, message=nil)
-    _wrap_assertion do
-      assert_block("assert<classic> should not be called with a block.") { !block_given? }
-      assert_block(build_message(message, "<?> is not true.", boolean)) { boolean }
-    end
-  end
-
   def reflect(diagnostic = nil, got = nil, called = caller[0],
                 options = {},
                 &block)
