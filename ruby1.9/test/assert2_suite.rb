4b35c99fed816d14e83fef0e9f45758799ee26b7
Kevin Miller <phlip@rubyforge.org>
2008-12-13 03:40:22 +0000

    development
    git-svn-id: http://assert2.rubyforge.org/svn@160 c6f06dc1-0964-42b6-8008-9d063af89333

--- ruby1.9/test/assert2_suite.rb
+++ ruby1.9/test/assert2_suite.rb
@@ -287,5 +287,63 @@ Error Handling
     deny{ message =~ /SyntaxError/ }
   end
 
+  def test_multi_line_assertion    
+    assert_flunk /false.*nil/m do
+      assert do
+        false
+        42; nil
+      end
+    end
+  end
+  
+  def morgothrond(thumpwhistle)
+    return false  #  what did you think such a function would do?? (-:
+  end
+  
+  def test_morgothrond_thumpwhistle
+    thumpwistle = 42
+    
+    x = assert_raise FlunkError do
+      assert{ self.morgothrond(thumpwistle) }
+    end
+    
+    assert{ x.message =~ /thumpwistle\s+--> 42/ }
+    denigh{ x.message =~ /self.morgothrond\s+--> / }
+  end
+
+  def test_assert
+    assert_flunk /x == 42.*false.*x \s*--> 43/m do
+      x = 43
+      assert{ x == 42 }
+    end
+  end
+
+  def test_deny_everything
+    assert_flunk /x.*true.*\s+--> 42/m do
+      x = 42
+      deny{ x == 42 }
+    end
+  end
+
+  def test_assert_classic
+    assert_flunk /(false. is not true)|(Failed assertion)/ do
+      assert false
+    end
+  end
+
+  def test_assertion_diagnostics
+    tattle = "doc says what's the condition?"
+    
+    assert_flunk /the condition.*tattle/m do
+      x = 43
+      assert(tattle){ x == 42 }
+    end
+
+    assert_flunk /on a mission/m do
+      x = 42
+      deny("I'm a man that's on a mission"){ x == 42 }
+    end
+  end
+
 end
 
