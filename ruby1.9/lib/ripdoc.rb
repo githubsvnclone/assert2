3dfc29f49ed85a32d0a2c5feeb04314563347667
Kevin Miller <phlip@rubyforge.org>
2008-12-14 02:48:15 +0000

    development
    git-svn-id: http://assert2.rubyforge.org/svn@170 c6f06dc1-0964-42b6-8008-9d063af89333

--- ruby1.9/lib/ripdoc.rb
+++ ruby1.9/lib/ripdoc.rb
@@ -90,8 +90,11 @@ class Ripdoc < Ripper::Filter
   end
   
   def name_toggle(banner)
+#p banner
     banner = banner.scan(/[[:alnum:]]/).join
-    return "<a name='#{banner}' id='#{banner}'></a>" # CONSIDER this can't use <a ../> because enline() would mangle it...
+    return "<a name='#{banner}' id='#{banner}_'></a>" # CONSIDER this can't use <a ../> because enline() would mangle it...
+      #  CONSIDER  the _ is due to a bug in libxml - it squeaks at a name and id that are the same - 
+       #                                     despite that's a common idiom!
   end
   
   def end_panel(f)
