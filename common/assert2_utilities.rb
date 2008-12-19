ca026b760bf109eff3a0a2b8b167b3beeaf3e6e5
Kevin Miller <phlip@rubyforge.org>
2008-12-19 05:41:01 +0000

    development
    git-svn-id: http://assert2.rubyforge.org/svn@262 c6f06dc1-0964-42b6-8008-9d063af89333

--- common/assert2_utilities.rb
+++ common/assert2_utilities.rb
@@ -42,8 +42,7 @@ module Test; module Unit; module Assertions
       @__additional_diagnostics[x] = d.call if d.respond_to? :call
     end
   end  #  TODO  pass the same args as blocks take
-      #  TODO  add a lite stack trace when assert{} or deny{} rescue
-
+      
   def assert(*args, &block)
   #  This assertion calls a block, and faults if it returns
   #  +false+ or +nil+. The fault diagnostic will reflect the
