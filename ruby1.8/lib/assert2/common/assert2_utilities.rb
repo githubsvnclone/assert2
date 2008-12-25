6c98bfc5f6eac204adce3e0212aa5d3343a79d00
Kevin Miller <phlip@rubyforge.org>
2008-12-25 01:25:29 +0000

    moving folders
    git-svn-id: http://assert2.rubyforge.org/svn@286 c6f06dc1-0964-42b6-8008-9d063af89333

---
+++ ruby1.8/lib/assert2/common/assert2_utilities.rb
@@ -0,0 +1,85 @@
+require 'test/unit'
+
+#  FIXME  move common into assert2
+
+module Test; module Unit; module Assertions
+
+  # This is a copy of the classic assert, so your pre-existing
+  # +assert+ calls will not change their behavior
+  #
+  if self.respond_to? :_assertions
+    def assert_classic(test, msg=nil)
+        msg ||= "Failed assertion, no message given."
+        self._assertions += 1
+        unless test then
+          msg = msg.call if Proc === msg
+          raise MiniTest::Assertion, msg
+        end
+        true
+    end
+  else
+    def assert_classic(boolean, message=nil)
+      #_wrap_assertion do
+        assert_block("assert<classic> should not be called with a block.") { !block_given? }
+        assert_block(build_message(message, "<?> is not true.", boolean)) { boolean }
+      #end
+    end
+  end
+
+  def add_diagnostic(whatever = nil, &block)
+    @__additional_diagnostics ||= []
+    
+    if whatever == :clear
+      @__additional_diagnostics = []
+      whatever = nil
+    end
+    
+    @__additional_diagnostics += [whatever, block]  # note .compact will take care of them if they don't exist
+  end
+
+  def __evaluate_diagnostics
+    @__additional_diagnostics.each_with_index do |d, x|
+      @__additional_diagnostics[x] = d.call if d.respond_to? :call
+    end
+  end  #  TODO  pass the same args as blocks take
+      
+  def assert(*args, &block)
+  #  This assertion calls a block, and faults if it returns
+  #  +false+ or +nil+. The fault diagnostic will reflect the
+  #  assertion's complete source - with comments - and will
+  #  reevaluate the every variable and expression in the
+  #  block.
+  #
+  #  The first argument can be a diagnostic string:
+  #
+  #    assert("foo failed"){ foo() }
+  #
+  #  The fault diagnostic will print that line.
+  # 
+  #  The next time you think to write any of these assertions...
+  #  
+  #  - +assert+
+  #  - +assert_equal+
+  #  - +assert_instance_of+
+  #  - +assert_kind_of+
+  #  - +assert_operator+
+  #  - +assert_match+
+  #  - +assert_not_nil+
+  #  
+  #  use <code>assert{ 2.1 }</code> instead.
+  #
+  #  If no block is provided, the assertion calls +assert_classic+,
+  #  which simulates RubyUnit's standard <code>assert()</code>.
+    if block
+      assert_ *args, &block
+    else
+      assert_classic *args
+    end
+    return true # or die trying ;-)
+  end
+
+  alias denigh deny  #  to line assert{ ... } and 
+                     #          denigh{ ... } statements up neatly!
+
+end ; end ; end
+
---
+++ ruby1.8/lib/assert2/common/assert_xhtml.rb
@@ -0,0 +1,103 @@
+require 'test/unit'
+#require 'xml'
+require 'rexml/document'
+require 'rexml/entity'
+require 'rexml/formatters/pretty'
+
+module Test; module Unit; module Assertions
+  
+  def assert_xhtml(xhtml)
+    return _assert_xml(xhtml) # , XML::HTMLParser)
+  end 
+
+  def _assert_xml(xml) #, parser = XML::Parser)
+    if false
+      xp = parser.new()
+      xp.string = xml
+      
+      if XML.respond_to? :'default_pedantic_parser='
+        XML.default_pedantic_parser = true
+      else
+        XML::Parser.default_pedantic_parser = true
+      end  #  CONSIDER  uh, figure out the best libxml-ruby??
+      
+      @xdoc = xp.parse.root
+    else
+      #  CONSIDER  figure out how entities are supposed to work!!
+      xml = xml.gsub('&mdash;', '--')
+      doc = REXML::Document.new(xml)
+      @xdoc = doc.root
+    end
+  end 
+
+  class AssertXPathArguments
+    
+    def initialize()
+      @subs = {}
+      @xpath = ''
+    end
+    
+    attr_reader :subs
+    attr_reader :xpath
+    
+    def to_conditions(hash)
+      xml_attribute_name = /^[a-z][_a-z0-9]+$/i  #  CONSIDER is that an XML attribute name match?
+      
+      @xpath << hash.map{|k, v|
+                  sk = k.to_s
+                  sk = '_text' if sk == '.'
+                  @subs[sk] = v.to_s
+                  "#{ '@' if k.to_s =~ xml_attribute_name }#{k} = $#{sk}" 
+                }.join(' and ')
+    end
+    
+    def to_predicate(hash, options)
+      hash = { :id => hash } if hash.kind_of? Symbol
+      hash.merge! options
+      @xpath << '[ '
+      to_conditions(hash)
+      @xpath << ' ]'
+    end
+
+    def to_xpath(path, id, options)
+      @xpath = path
+      @xpath = "descendant-or-self::#{ @xpath }" if @xpath.kind_of? Symbol
+      to_predicate(id, options) if id
+    end
+
+  end
+
+    # if node = @xdoc.find_first(path) ## for libxml
+    #  def node.text
+    #    find_first('text()').to_s
+    #  end
+
+  def xpath(path, id = nil, options = {}, &block)
+    former_xdoc = @xdoc
+    apa = AssertXPathArguments.new
+    apa.to_xpath(path, id, options)
+    node = REXML::XPath.first(@xdoc, apa.xpath, nil, apa.subs)
+    
+    add_diagnostic :clear do
+      bar = REXML::Formatters::Pretty.new
+      out = String.new
+      bar.write(@xdoc, out)
+      diagnostic = "xpath: #{ apa.xpath.inspect }\n"
+      diagnostic << "arguments: #{ apa.subs.pretty_inspect }\n" if apa.subs.any?
+      diagnostic + "xml context:\n" + out
+    end
+    
+    assert_ nil, :args => [@xdoc = node], &block if node and block
+    return node
+    # TODO raid http://thebogles.com/blog/an-hpricot-style-interface-to-libxml/
+  ensure
+    @xdoc = former_xdoc
+  end  #  FIXME trap LibXML::XML::XPath::InvalidPath and explicate it's an XPath problem
+ 
+  def indent_xml
+    @xdoc.write($stdout, 2)
+  end
+  
+end; end; end
+
+require '../../test/assert_xhtml_suite.rb' if $0 == __FILE__ and File.exist?('../../test/assert_xhtml_suite.rb')
