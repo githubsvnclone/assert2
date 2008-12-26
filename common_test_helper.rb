e12c64a0343fac4629c5f396859bcd1e17098597
Kevin Miller <phlip@rubyforge.org>
2008-12-26 15:55:46 +0000

    development
    git-svn-id: http://assert2.rubyforge.org/svn@336 c6f06dc1-0964-42b6-8008-9d063af89333

--- common_test_helper.rb
+++ common_test_helper.rb
@@ -2,11 +2,12 @@ require 'pathname'
 
 HomePath = (Pathname.new(__FILE__).dirname + '..').expand_path
 Ruby186 = (HomePath + '../ruby1.8').expand_path
+Ruby190 = (HomePath + '../ruby1.9').expand_path
 
 if RUBY_VERSION < '1.9.0'
   $:.unshift Ruby186 + 'lib'  #  reach out to ruby1.8's assert{ 2.0 }
 else
-  $:.unshift HomePath + 'lib'  #  reach out to ruby1.9's assert{ 2.1 }
+  $:.unshift Home190 + 'lib'  #  reach out to ruby1.9's assert{ 2.1 }
   require 'ripdoc'
 end
 
