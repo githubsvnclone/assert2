9f59d5ac562734701d5d5632a4fd2468fb76ec18
Kevin Miller <phlip@rubyforge.org>
2008-12-27 01:39:58 +0000

    development
    git-svn-id: http://assert2.rubyforge.org/svn@363 c6f06dc1-0964-42b6-8008-9d063af89333

--- common/assert2_utilities.rb
+++ common/assert2_utilities.rb
@@ -84,9 +84,6 @@ module Test; module Unit; module Assertions
     return true # or die trying ;-)
   end
 
-  alias denigh deny  #  to line assert{ ... } and 
-                     #          denigh{ ... } statements up neatly!
-
   module Coulor  #:nodoc:
     def colorize(we_color)
       @@we_color = we_color
@@ -130,6 +127,58 @@ module Test; module Unit; module Assertions
   def colorize(to_color)
     RubyReflector.new.colorize(to_color)
   end
+
+  #  The new <code>assert()</code> calls this to interpret
+  #  blocks of assertive statements.
+  #
+  def assert_(diagnostic = nil, options = {}, &block)
+    @__additional_diagnostics = []
+    
+    begin
+      got = block.call(*options[:args]) and return got
+    rescue FlunkError
+      raise  #  asserts inside assertions that fail do not decorate the outer assertion
+    rescue => got
+      add_exception got
+    end
+
+    flunk diagnose(diagnostic, got, caller[1], options, block)
+  end
+
+  def add_exception(ex)
+    ex.backtrace[0..10].each do |line|
+      add_diagnostic '  ' + line
+    end
+  end
+
+  #  This assertion replaces:
+  #  
+  #  - +assert_nil+
+  #  - +assert_no_match+
+  #  - +assert_not_equal+
+  #
+  #  It faults, and prints its block's contents and values,
+  #  if its block returns non-+false+ and non-+nil+.
+  #  
+  def deny(diagnostic = nil, options = {}, &block)
+      #  "None shall pass!" --the Black Knight
+      
+    @__additional_diagnostics = []
+    
+    begin
+      got = block.call(*options[:args]) or return true
+    rescue FlunkError
+      raise
+    rescue => got
+      add_exception got
+    end
   
+    flunk diagnose(diagnostic, got, caller[0], options, block)
+  end  #  "You're a looney!"  -- King Arthur
+
+  alias denigh deny  #  to line assert{ ... } and 
+                     #          denigh{ ... } statements up neatly!
+                     #  FIXME  the require lines for this file needn't be so low now
+
 end ; end ; end
 
