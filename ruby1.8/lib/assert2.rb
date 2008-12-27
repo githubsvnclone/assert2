a25000381f6de2b343f0c053e0bae75084833f35
Kevin Miller <phlip@rubyforge.org>
2008-12-27 01:34:34 +0000

    development
    git-svn-id: http://assert2.rubyforge.org/svn@361 c6f06dc1-0964-42b6-8008-9d063af89333

--- ruby1.8/lib/assert2.rb
+++ ruby1.8/lib/assert2.rb
@@ -10,45 +10,6 @@ require 'assert2/ruby_reflector'
 
 module Test; module Unit; module Assertions
 
-  #  The new <code>assert()</code> calls this to interpret
-  #  blocks of assertive statements.
-  #
-  def assert_(diagnostic = nil, options = {}, &block)
-    @__additional_diagnostics = []
-    begin
-      got = block.call(*options[:args]) and return got
-    rescue FlunkError
-      raise  #  asserts inside assertions that fail do not decorate the outer assertion
-    rescue => got
-    end
-
-    flunk diagnose(diagnostic, got, caller[1], options, block)
-  end
-
-  #  This assertion replaces:
-  #
-  #  - +assert_nil+
-  #  - +assert_no_match+
-  #  - +assert_not_equal+
-  #
-  #  It faults, and prints its block's contents and values,
-  #  if its block returns non-+false+ and non-+nil+.
-  #
-  def deny(diagnostic = nil, options = {}, &block)  
-      #  "None shall pass!" --the Black Knight
-
-    @__additional_diagnostics = []
-
-    begin
-      got = block.call(*options[:args]) or return true
-    rescue FlunkError
-      raise
-    rescue => got
-    end
-
-    flunk diagnose(diagnostic, got, caller[0], options, block)
-  end  #  "You're a looney!"  -- King Arthur
-
   # Assert that a block raises a given Exception type matching 
   # a given message
   # 
@@ -68,7 +29,7 @@ module Test; module Unit; module Assertions
     args = [types].flatten + [diagnostic]
     exception = assert_raise(*args, &block)
     
-    assert_match matcher,
+    assert_match matcher,  #  TODO  merge this stuff into the utilities
                  exception.message,
                  [ diagnostic, 
                    "incorrect #{ exception.class.name 
