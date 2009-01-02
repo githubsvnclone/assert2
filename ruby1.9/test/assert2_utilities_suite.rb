9721ea13f6c61a749564584229b1d3d97725bd33
Kevin Miller <phlip@rubyforge.org>
2009-01-02 23:58:16 +0000

    development
    git-svn-id: http://assert2.rubyforge.org/svn@482 c6f06dc1-0964-42b6-8008-9d063af89333

--- ruby1.9/test/assert2_utilities_suite.rb
+++ ruby1.9/test/assert2_utilities_suite.rb
@@ -174,12 +174,18 @@ class Assert2UtilitiesSuite < Test::Unit::TestCase
 
   def test_format_inspection
     rf = RubyReflector.new
-    assert{ rf.format_inspection('foo', 42) == 'foo' }
+    assert{ rf.format_inspection('foo'.inspect, 42) == '"foo"' }
   end
 
   def test_format_multiline_inspection
     rf = RubyReflector.new
     assert{ rf.format_inspection("foo\nbar", 42) =~ /foo\n\s{42}bar/ }
+  end
+
+  def test_format_literal_multiline_inspection
+    rf = RubyReflector.new
+    return # FIXME
+    assert{ rf.format_inspection("foo\\nbar", 42) =~ /foo\n\s{42}bar/ }
   end
 
 end  #  TODO  ultimately, the top of _this_ file should document all of assert{ 2.x }
