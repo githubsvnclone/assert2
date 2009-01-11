a3fabb80a9976fba99e0a9f99e1eb5a5fc4087b3
Kevin Miller <phlip@rubyforge.org>
2009-01-11 22:44:01 +0000

    retiring old system
    git-svn-id: http://assert2.rubyforge.org/svn@607 c6f06dc1-0964-42b6-8008-9d063af89333

--- ruby1.9/lib/assert2.rb
+++
@@ -1,9 +0,0 @@
-
-require 'assert2/common/assert2_utilities'
-
-
-end; end; end
-
-require '../test/assert2_suite.rb' if $0 == __FILE__ and File.exist?('../test/assert2_suite.rb')
-#require 'ripdoc_suite.rb' if $0 == __FILE__ and File.exist?('ripdoc_suite.rb')
-
