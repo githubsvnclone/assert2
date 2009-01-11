396bf7766ca95fae77821da47cf6ecdec61da2b5
Kevin Miller <phlip@rubyforge.org>
2009-01-11 22:18:33 +0000

    development
    git-svn-id: http://assert2.rubyforge.org/svn@603 c6f06dc1-0964-42b6-8008-9d063af89333

--- test/assert2_suite.rb
+++ test/assert2_suite.rb
@@ -349,6 +349,49 @@ to recover some 1.8.6 stability!
     deny{ x == 42 }
   end
 
+  def test_catch_exceptions
+    x = 42
+    return unless RubyReflector::HAS_RUBYNODE
+
+    assert_flunk /RuntimeError.*gotcha.*42/m do
+      assert{ x; raise 'gotcha' }
+    end
+
+    assert_flunk /look out.*RuntimeError.*gotcha.*42/m do
+      assert('look out!'){ x; raise 'gotcha' }
+    end
+  end
+
+  def test_catch_undeniable_exceptions
+    x = 42
+    return unless RubyReflector::HAS_RUBYNODE
+
+    assert_flunk /RuntimeError.*me_too.*42/m do
+      denigh{ x; raise 'me_too' }
+    end
+
+    assert_flunk /tolja.*RuntimeError.*me_too.*42/m do
+      deny('tolja'){ x; raise 'me_too' }
+    end
+  end
+
+  def test_flunk_2_0
+# FIXME    return unless RubyReflector::HAS_RUBYNODE
+    x = 43
+    assert_flunk /43.*but.*42/m    do  assert_equal x, 42          end
+    assert_flunk /x == 42/         do  assert{ x == 42 }           end
+    assert_flunk /43.*expect.*43/m do  assert_not_equal x, 43      end
+    assert_flunk /x > 43/          do  assert{ x > 430 }           end
+    assert_flunk /nope/            do  assert('nope'){ x > 430 }   end
+    assert_flunk /x.* > 43/ do  assert x > 430, 'x should be > 43'  end
+
+    return unless RubyReflector::HAS_RUBYNODE
+    
+    assert_flunk /x == 43.*should not.*x.*43/m do
+      deny{ x == 43 }
+    end
+  end
+
   #############################################################
   ######## for manual tests
 
