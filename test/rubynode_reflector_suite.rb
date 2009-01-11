d0aea15d106810f6b7da050e0a5102dca5a3e68c
Kevin Miller <phlip@rubyforge.org>
2009-01-11 19:58:35 +0000

    development
    git-svn-id: http://assert2.rubyforge.org/svn@574 c6f06dc1-0964-42b6-8008-9d063af89333

--- test/rubynode_reflector_suite.rb
+++ test/rubynode_reflector_suite.rb
@@ -13,7 +13,7 @@ class RubyReflectorTest < Test::Unit::TestCase
 #  FIXME  move stuff everyone should pass to a common suite!
 
   def test_pass_args_to_detector
-    return # FIXME
+    # return # FIXME
     @effect.captured_block_vars = 'x, y'
     @effect.args = [40, 2]
     assert{ @effect.detect('x + y').include? ['x + y', 42, nil] }
