b02f23e5952c977946d7307547e0a94409474f6c
Kevin Miller <phlip@rubyforge.org>
2009-01-11 19:00:05 +0000

    development
    git-svn-id: http://assert2.rubyforge.org/svn@567 c6f06dc1-0964-42b6-8008-9d063af89333

--- test/assert2_suite.rb
+++ test/assert2_suite.rb
@@ -297,7 +297,6 @@ to recover some 1.8.6 stability!
   end
 
   def test_adjust_linefeeds_in_diagnostics
-    return # FIXME
     diagnostic = assert_flunk /nope/ do
                   x = "line with\nlinefeed"
                   assert{ x =~ /nope/ }
