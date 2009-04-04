ceb6422817688d4a972e938244f74de814758a10
Kevin Miller <phlip@rubyforge.org>
2009-04-04 04:57:08 +0000

    development
    git-svn-id: http://assert2.rubyforge.org/svn@925 c6f06dc1-0964-42b6-8008-9d063af89333

--- lib/assert2/rjs.rb
+++ lib/assert2/rjs.rb
@@ -7,27 +7,41 @@ require 'assert2/xhtml'
 
 module Test; module Unit; module Assertions
 
-  def assert_rjs(command, target, matcher = //, &block)
-    ast = RKelly.parse(@response.body)
-    command == :replace_html or 
-      flunk("assert_rjs's alpha version only respects :replace_html")
+  class AssertRjs
+    def initialize(js); @ast = RKelly.parse(@js = js); end
     
-    ast.pointcut('Element.update()').matches.each do |updater|
-      updater.grep(RKelly::Nodes::ArgumentsNode).each do |thang|
-        div_id, html = thang.value
-        
-        if target and html
-          div_id = eval(div_id.value)
-          html   = eval(html.value)
-          if div_id == target.to_s
-            assert_match matcher, html
-            assert_xhtml html, &block if block
-            return html
+    def replace_html(command, target, &block)
+      @ast.pointcut('Element.update()').matches.each do |updater|
+        updater.grep(RKelly::Nodes::ArgumentsNode).each do |thang|
+          div_id, html = thang.value
+          
+          if target and html
+            div_id = eval(div_id.value)
+            html   = eval(html.value)
+            if div_id == target.to_s
+              block.call(div_id, html)
+            end
           end
         end
       end
+      return false
     end
-    flunk 'TODO'
+  end
+
+  def assert_rjs(command, target, matcher = //, &block)
+    rjs = AssertRjs.new(js = @response.body)
+    
+    command == :replace_html or  #  TODO  put me inside the method_missing!
+      flunk("assert_rjs's alpha version only respects :replace_html")
+      
+    rjs.send command, command, target do |div_id, html|
+      assert_match matcher, html, 
+  "#{ command } for ID #{ target } has incorrect payload, in #{ js }"
+      assert_xhtml html, &block if block
+      return html
+    end
+    
+    flunk "#{ command } for ID #{ target } not found in #{ js }"
   end
 
 end; end; end
