0a4757f4ce97a3f51b5f09a91a59814fec32046f
Kevin Miller <phlip@rubyforge.org>
2009-04-04 04:02:39 +0000

    development
    git-svn-id: http://assert2.rubyforge.org/svn@922 c6f06dc1-0964-42b6-8008-9d063af89333

--- lib/assert2/rjs.rb
+++ lib/assert2/rjs.rb
@@ -3,12 +3,28 @@ require 'rkelly'
 
 module Test; module Unit; module Assertions
 
-  def assert_rjs(command, target)
+  def assert_rjs(command, target, matcher = //)
     ast = RKelly.parse(@response.body)
-      #  TODO  visit the parsed code via the command, and assert it, here
-#puts ast.public_methods.sort
-
-    p element = ast.pointcut('Element').matches.first
+    command == :replace_html or 
+      flunk("assert_rjs's alpha version only respects :replace_html")
+    
+    ast.pointcut('Element.update()').matches.each do |updater|
+      updater.grep(RKelly::Nodes::ArgumentsNode).each do |thang|
+        div_id, html = thang.value
+        
+        if target and html
+          div_id = eval(div_id.value)
+          html   = eval(html.value)
+p target
+p html
+          if div_id == target.to_s
+            assert_match matcher, html
+            return html
+          end
+        end
+      end
+    end
+    flunk 'TODO'
   end
 
 end; end; end
--- test/assert2_rjs_suite.rb
+++ test/assert2_rjs_suite.rb
@@ -8,6 +8,7 @@ class AssertRjsSuite < Test::Unit::TestCase
     @response = OpenStruct.new(:body => "Element.update(\"label_7\", \"<input checked=\\\"checked\\\" id=\\\"Top_Ranking\\\" name=\\\"Top_Ranking\\\" type=\\\"checkbox\\\" value=\\\"Y\\\" \\/>I want a pet &lt; than a chihuahua<input id=\\\"cross_sale_1\\\" name=\\\"cross_sale_1\\\" type=\\\"hidden\\\" value=\\\"7\\\" \\/>\");")
     
     assert_rjs :replace_html, :label_7
+    assert_rjs :replace_html, :label_7, /Top_Ranking/
   end
   
 end
